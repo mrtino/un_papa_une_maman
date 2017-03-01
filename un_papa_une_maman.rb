5cb68ed60c8cd0c8108941d89f42b71cee1af18f
Alexis Viguié <alexis.viguie@epitech.eu>
2017-03-01 20:09:11 +0100

    rely on env variables rather than a file for api keys

--- un_papa_une_maman.rb
+++ un_papa_une_maman.rb
@@ -5,29 +5,20 @@
 require 'twitter'
 
 # Ne pas lancer le script s'il y a moins de trois arguments
-abort "Merci de spécifier deux fichiers comportant respectivement des noms masculins et féminins séparés par des lignes, ainsi qu'un fichier avec vos clés d'API." unless ARGV.size >= 3
+abort "Merci de spécifier deux fichiers comportant respectivement des noms masculins et féminins séparés par des lignes." unless ARGV.size == 2
 
 # Ne pas lancer le script si les fichiers spécifiés n'existent pas, sont des dossiers ou ne sont pas lisibles
 ARGV.each do |arg|
   abort "Impossible d'ouvrir le fichier #{arg}." unless File.readable? arg
 end
 
-# Ne pas lancer le script si le fichier de clés est incorrect (contient moins de 4 lignes)
-abort "Fichiers de clés incorrect." unless `wc -l #{ARGV[2]}`.strip.split(' ')[0].to_i < 4
-
-# Ouverture du fichiers de clés
-keys_file = File.new(ARGV[2], "r")
-
 # Parsing du fichiers de clés afin de configurer le client
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = keys_file.gets.strip
-  config.consumer_secret     = keys_file.gets.strip
-  config.access_token        = keys_file.gets.strip
-  config.access_token_secret = keys_file.gets.strip
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
 end
-
-# Fermeture du fichier de clés
-keys_file.close
 
 papa_maman = [ "papa", "maman" ]        # Définition des deux mots par défaut
 rand_num = rand(2)                      # Choisit aléatoirement quel mot va se faire remplacer
